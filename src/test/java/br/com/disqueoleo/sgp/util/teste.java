4184e7850ff5a361c37030c496a4fbea7084ed19
celsojr <croclaro@yahoo.com.br>
2021-10-22 21:57:02 -0300

    Atualizado páginas fornecedores e afiliados

--- src/test/java/br/com/disqueoleo/sgp/util/teste.java
+++ src/test/java/br/com/disqueoleo/sgp/util/teste.java
@@ -1,33 +1,42 @@
 package br.com.disqueoleo.sgp.util;
 
-import org.apache.shiro.crypto.hash.SimpleHash;
-import org.junit.Test;
+import java.util.List;
 
-import br.com.disqueoleo.sgp.dao.FuncionarioDAO;
-import br.com.disqueoleo.sgp.dao.UsuarioDAO;
-import br.com.disqueoleo.sgp.domain.Funcionario;
-import br.com.disqueoleo.sgp.domain.Usuario;
+import br.com.disqueoleo.sgp.dao.FornecedorDAO;
+import br.com.disqueoleo.sgp.domain.Fornecedor;
 
 public class teste {
-	@Test
-	public void salvar(){
-		FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-		Funcionario funcionario = funcionarioDAO.buscar(1l);
+	/*
+	 * @Test public void salvar(){ FuncionarioDAO funcionarioDAO = new
+	 * FuncionarioDAO(); Funcionario funcionario = funcionarioDAO.buscar(1l);
+	 * 
+	 * Usuario usuario = new Usuario(); usuario.setStatus(true);
+	 * usuario.setFuncionario(funcionario);
+	 * usuario.setDataUsuario("12/-12/2017 06:00:00"); usuario.setSenha("123");
+	 * 
+	 * SimpleHash hash = new SimpleHash("md5", usuario.getSenha());
+	 * usuario.setSenha(hash.toHex());
+	 * 
+	 * UsuarioDAO usuarioDAO = new UsuarioDAO(); usuarioDAO.salvar(usuario);
+	 * 
+	 * System.out.println("SALVO COM SUCESSO");
+	 * 
+	 * 
+	 * }
+	 */
+	
+	public static void main(String[] args) {
 		
-		Usuario usuario = new Usuario();
-		usuario.setStatus(true);
-		usuario.setFuncionario(funcionario);
-		usuario.setDataUsuario("12/-12/2017 06:00:00");
-		usuario.setSenha("123");
+		Fornecedor fornecedor = null;
+		List<Fornecedor> fornecedores = null;
 		
-		SimpleHash hash = new SimpleHash("md5", usuario.getSenha());
-		usuario.setSenha(hash.toHex());
+		int i = 0;
 		
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		usuarioDAO.salvar(usuario);
-		
-		System.out.println("SALVO COM SUCESSO");
+		while(i<0) {
+			FornecedorDAO fornecedorDAO = new FornecedorDAO();
+			fornecedores.add(fornecedor);
+			i++;
+		}
 	}
-	
-	
+
 }
