845745a83bf8fae82eca4d008288ad8557ff12fb
celsojr <croclaro@yahoo.com.br>
2021-11-08 11:31:47 -0300

    Impressão PDF e Excel resolvidos

--- src/main/java/br/com/disqueoleo/sgp/bean/RelatoriosBean.java
+++ src/main/java/br/com/disqueoleo/sgp/bean/RelatoriosBean.java
@@ -1,5 +1,4 @@
 package br.com.disqueoleo.sgp.bean;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
@@ -10,7 +9,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
@@ -29,98 +27,54 @@ import com.lowagie.text.PageSize;
 
 import br.com.disqueoleo.sgp.dao.BancoDAO;
 import br.com.disqueoleo.sgp.domain.Banco;
+import br.com.disqueoleo.sgp.domain.Fornecedor;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class ConsultaBancosBean implements Serializable {
-	private Banco banco;
-	private List<Banco> bancos;	
-
+public class RelatoriosBean implements Serializable{
+	public Banco banco;	
+	public Fornecedor fornecedor;
+	public List<Fornecedor> fornecedores;
+	public List<Banco> bancos;
 	public Banco getBanco() {
 		return banco;
 	}
-
 	public void setBanco(Banco banco) {
 		this.banco = banco;
 	}
-
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+	public List<Fornecedor> getFornecedores() {
+		return fornecedores;
+	}
+	public void setFornecedores(List<Fornecedor> fornecedores) {
+		this.fornecedores = fornecedores;
+	}
 	public List<Banco> getBancos() {
 		return bancos;
-	}	
-
+	}
 	public void setBancos(List<Banco> bancos) {
 		this.bancos = bancos;
 	}
-
+	
 	@PostConstruct
 	public void listar() {
 		try {
 
 			BancoDAO bancoDAO = new BancoDAO();
-			bancos = bancoDAO.listar();				
+			bancos = bancoDAO.listar();	
+			
 
 		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar listar os banco");
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os dados");
 			erro.printStackTrace();
 		}
 	}
-
-	public void cadastrar() {
-		try {
-			banco = new Banco();
-			BancoDAO bancoDAO = new BancoDAO();
-			bancos = bancoDAO.listar();
-
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao cadastrar o banco!!!");
-			erro.printStackTrace();
-		}
-	}
-
-	public void salvar() {
-		try {
-			BancoDAO bancoDAO = new BancoDAO();
-			bancoDAO.merge(banco);
-
-			cadastrar();
-
-			Messages.addGlobalInfo("O Banco foi editado com sucesso!!!");
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao editar o banco!!!");
-			erro.printStackTrace();
-		}
-	}
-
-	public void excluir(ActionEvent evento) {
-		try {
-			banco = (Banco) evento.getComponent().getAttributes().get("bancoSelecionado");
-
-			BancoDAO bancoDAO = new BancoDAO();
-			bancoDAO.excluir(banco);
-
-			bancos = bancoDAO.listar();
-
-			Messages.addGlobalInfo("Banco excluido com sucesso.");
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Erro ao excluir o Banco.");
-			erro.printStackTrace();
-		}
-	}
-
-	public void editar(ActionEvent evento) {
-		try {
-			banco = (Banco) evento.getComponent().getAttributes().get("bancoSelecionado");
-
-			BancoDAO bancoDAO = new BancoDAO();
-			bancos = bancoDAO.listar();
-
-			Messages.addGlobalInfo("Banco editado com sucesso");
-		} catch (RuntimeException erro) {
-			erro.printStackTrace();
-		}
-	}
-	
 	
 	public void postProcessXLS(Object document) {
 		HSSFWorkbook wb = (HSSFWorkbook) document;
@@ -142,15 +96,17 @@ public class ConsultaBancosBean implements Serializable {
 		Document pdf = (Document) document;
 		
 		pdf.setPageSize(PageSize.LEGAL.rotate());
-		pdf.setMargins(0, 0, 0, 0);
+		pdf.setMargins(2, 2, 2, 0);
 		pdf.open();
 				
 		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 
 		String separator = File.separator;
 		String logo = externalContext.getRealPath("") + separator + "resources" + separator + "images" + separator
-				+ "LogoDisqueOleo1.png";
+				+ "logo.png";
 
 		pdf.add(Image.getInstance(logo));		
 	}
+	
+	
 }
