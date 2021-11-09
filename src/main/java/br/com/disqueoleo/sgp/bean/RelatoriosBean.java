c0d51c3a600e17a38dc3f1fa3e8ed999ea093059
celsojr <croclaro@yahoo.com.br>
2021-11-09 13:11:33 -0300

    CORREÇÃO DE GERAÇÃO DE PDF E EXCEL RELATÓRIOS

--- src/main/java/br/com/disqueoleo/sgp/bean/RelatoriosBean.java
+++ src/main/java/br/com/disqueoleo/sgp/bean/RelatoriosBean.java
@@ -1,14 +1,13 @@
 package br.com.disqueoleo.sgp.bean;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
@@ -17,65 +16,20 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.FillPatternType;
-import org.omnifaces.util.Messages;
 
 import com.lowagie.text.BadElementException;
+import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
-
-import br.com.disqueoleo.sgp.dao.BancoDAO;
-import br.com.disqueoleo.sgp.domain.Banco;
-import br.com.disqueoleo.sgp.domain.Fornecedor;
+import com.lowagie.text.pdf.PdfPTable;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class RelatoriosBean implements Serializable{
-	public Banco banco;	
-	public Fornecedor fornecedor;
-	public List<Fornecedor> fornecedores;
-	public List<Banco> bancos;
-	public Banco getBanco() {
-		return banco;
-	}
-	public void setBanco(Banco banco) {
-		this.banco = banco;
-	}
-	public Fornecedor getFornecedor() {
-		return fornecedor;
-	}
-	public void setFornecedor(Fornecedor fornecedor) {
-		this.fornecedor = fornecedor;
-	}
-	public List<Fornecedor> getFornecedores() {
-		return fornecedores;
-	}
-	public void setFornecedores(List<Fornecedor> fornecedores) {
-		this.fornecedores = fornecedores;
-	}
-	public List<Banco> getBancos() {
-		return bancos;
-	}
-	public void setBancos(List<Banco> bancos) {
-		this.bancos = bancos;
-	}
-	
-	@PostConstruct
-	public void listar() {
-		try {
+public class RelatoriosBean implements Serializable {
 
-			BancoDAO bancoDAO = new BancoDAO();
-			bancos = bancoDAO.listar();	
-			
-
-		} catch (RuntimeException erro) {
-			Messages.addGlobalError("Ocorreu um erro ao tentar listar os dados");
-			erro.printStackTrace();
-		}
-	}
-	
 	public void postProcessXLS(Object document) {
 		HSSFWorkbook wb = (HSSFWorkbook) document;
 		HSSFSheet sheet = wb.getSheetAt(0);
@@ -88,25 +42,42 @@ public class RelatoriosBean implements Serializable{
 		for (int i = 0; i < header.getPhysicalNumberOfCells(); i++) {
 			HSSFCell cell = header.getCell(i);
 
-			cell.setCellStyle(cellStyle);			
+			cell.setCellStyle(cellStyle);
 		}
 	}
 
 	public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
+		// Document pdf = (Document) document;
+		// pdf.open();
+		// Font catFont = new Font(Font.TIMES_ROMAN, 18, Font.BOLD);
+		// Paragraph p = new Paragraph("SGP - SISTEMA GERENCIADOR DE PRODUTOS",
+		// catFont);
+		// p.setAlignment(Element.ALIGN_CENTER);
+		// p.setSpacingAfter(20);
+		// pdf.add(p);
+		// HeaderFooter footer = new HeaderFooter(new Phrase("página "), new
+		// Phrase("."));
+		// pdf.setFooter(footer);
+
+		// pdf.setPageSize(PageSize.A4.rotate());
+		// pdf.setMargins(0f, 0f, 0f, 0f);
+		// pdf.open();
+
 		Document pdf = (Document) document;
-		
-		pdf.setPageSize(PageSize.LEGAL.rotate());
-		pdf.setMargins(2, 2, 2, 0);
+		pdf.setPageSize(PageSize.A4.rotate());
 		pdf.open();
-				
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 
-		String separator = File.separator;
-		String logo = externalContext.getRealPath("") + separator + "resources" + separator + "images" + separator
-				+ "logo.png";
+		ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext()
+				.getContext();
+		String logo = servletContext.getRealPath("") + File.separator + "resources" + File.separator + "images"
+				+ File.separator + File.separator + "logo.png";
+		Image image = Image.getInstance(logo);
 
-		pdf.add(Image.getInstance(logo));		
+		pdf.add(image);
+		// add a couple of blank lines		
+		pdf.add(Chunk.NEWLINE);
+		
+		
 	}
-	
-	
+
 }
