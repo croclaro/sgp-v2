c8a963dcb69fd0308a2333ac966f2b1ac78fb718
Sérgio Roberto Delfino <srdelfino@gmail.com>
2021-11-17 21:15:01 -0300

    Atualizações

--- src/main/java/br/com/disqueoleo/sgp/domain/EnviarEmail.java
+++ src/main/java/br/com/disqueoleo/sgp/domain/EnviarEmail.java
@@ -89,4 +89,89 @@ public class EnviarEmail {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public void enviarEmailFornecedor(Fornecedor fornecedor) {
+
+		Properties props = new Properties();
+		/** Parâmetros de conexão com servidor Gmail */
+		props.put("mail.smtp.host", "mail.sgp-disqueoleo.com.br");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("admin@sgp-disqueoleo.com.br", "C3lso1359@#");
+			}
+		});
+
+		/** Ativa Debug para sessão */
+		session.setDebug(true);
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("admin@sgp-disqueoleo.com.br"));
+			// Remetente
+
+			Address[] toUser = InternetAddress // Destinatário(s)
+					.parse(fornecedor.getEmail());
+
+			message.setRecipients(Message.RecipientType.TO, toUser);
+			message.setSubject("SGP - Sistema Gerenciador de Produtos");// Assunto
+			
+			StringBuilder texto = new StringBuilder();
+			texto.append("Seu email foi enviado por: ");
+			texto.append("http://localhost:8080/SGP/cad-fornecedorIndicadoUpgrade.xhtml?codigo=" + fornecedor.getCodigo());
+			message.setText(texto.toString());
+
+			/** Método para enviar a mensagem criada */
+			Transport.send(message);
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void enviarEmailUsuario(Usuario usuario) {
+
+		Properties props = new Properties();
+		/** Parâmetros de conexão com servidor Gmail */
+		props.put("mail.smtp.host", "mail.sgp-disqueoleo.com.br");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("admin@sgp-disqueoleo.com.br", "C3lso1359@#");
+			}
+		});
+
+		/** Ativa Debug para sessão */
+		session.setDebug(true);
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("admin@sgp-disqueoleo.com.br"));
+			// Remetente
+
+			Address[] toUser = InternetAddress // Destinatário(s)
+					.parse(usuario.getFornecedor().getEmail());
+
+			message.setRecipients(Message.RecipientType.TO, toUser);
+			message.setSubject("SGP - Sistema Gerenciador de Produtos");// Assunto
+			
+			StringBuilder texto = new StringBuilder();
+			texto.append("Token: " + usuario.getToken());
+			texto.append(" CNPJ: " + usuario.getFornecedor().getCnpj());
+			texto.append(" Senha Provisória: " + usuario.getSenhaSemCriptografia());
+			message.setText(texto.toString());
+
+			/** Método para enviar a mensagem criada */
+			Transport.send(message);
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
